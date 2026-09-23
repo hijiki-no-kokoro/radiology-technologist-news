3d1e801f2d29f5046beb8cbfb76ab9ba7f760965
hijiki-no-kokoro <zippertype@gmail.com>
2026-09-23 21:58:54 +0900

    Bump beer app cache version

--- beer-save/sw.js
+++ beer-save/sw.js
@@ -1,4 +1,4 @@
-const CACHE='beer-save-v3';
+const CACHE='beer-save-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
